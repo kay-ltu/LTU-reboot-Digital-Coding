914aeeb9c3fffd8252b5358bd1fdec84c84ec52c
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-24 11:29:24 +0000

    Create task-3.1.5.js
    Concatenated

---
+++ module-3/scripts/task-3.1.5.js
@@ -0,0 +1,13 @@
+var myFirstName;
+
+var myFirstName = 'K ';
+
+var myFullName;
+
+var myFullName = myFirstName + 'Hoyland';
+
+alert(myFullName);
+
+console.log(myFullName);
+
+document.write(myFullName);
