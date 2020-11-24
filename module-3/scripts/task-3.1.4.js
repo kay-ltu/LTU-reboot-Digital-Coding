fa7dd3a8f074f13cea061c1fcd7d4f32b2953c5e
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-24 11:18:31 +0000

    Create task-3.1.4.js
    Used values as algebra

---
+++ module-3/scripts/task-3.1.4.js
@@ -0,0 +1,17 @@
+var mySon;
+
+var mySon = '13';
+
+var myDaughter;
+
+var myDaughter = '11';
+
+var myKidsCombinedAge;
+
+var myKidsCombinedAge = mySon + myDaughter;
+
+alert(myKidsCombinedAge);
+
+console.log(myKidsCombinedAge);
+
+document.write(myKidsCombinedAge);
