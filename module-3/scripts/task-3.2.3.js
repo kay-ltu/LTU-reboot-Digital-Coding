9ae1c104378e0b78be9e43e52c8befd9215f4279
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-25 10:50:18 +0000

    Create task-3.2.3.js

---
+++ module-3/scripts/task-3.2.3.js
@@ -0,0 +1,7 @@
+function fullName( firstName, lastName){
+ return firstName + ' ' + lastName;
+}
+
+console.log ( fullName('Kirsty', 'Hoyland'));
+var nameOfPerson = fullName('Kirsty', 'Hoyland')
+console.log( nameOfPerson );
