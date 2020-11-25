8bcdf364abc28f32201ef1cac195830f9b82b795
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-25 13:39:21 +0000

    Create task-3.2.5.js

---
+++ module-3/scripts/task-3.2.5.js
@@ -0,0 +1,15 @@
+var temperature = 60;
+
+if (temperature < 0) {
+    console.log('stay inside')
+}
+
+else if (temperature < 30) {
+    console.log('wear a coat and a hat')
+}
+
+else if (temperature < 50) {
+    console.log('wear a coat')
+} else { 
+    console.log('just wear pants and a vest is fine');
+}
