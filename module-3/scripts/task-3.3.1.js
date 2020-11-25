bda07001d22d4d70c3e7a799a692f981bb0df488
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-25 14:11:35 +0000

    Update task-3.3.1.js

---
+++ module-3/scripts/task-3.3.1.js
@@ -0,0 +1,6 @@
+function percentageCalculator (number, percentage){
+    var result = (number * percentage) / 100;
+    return result;
+}
+console.log( percentageCalculator( 135, 30) );
+console.log( percentageCalculator( 207, 12.5) );
