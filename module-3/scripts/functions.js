b078342da5d7d4f0e4e45583f462bbce9a98a36a
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-25 09:50:14 +0000

    Create functions.js

---
+++ module-3/scripts/functions.js
@@ -0,0 +1,15 @@
+/*
+Delcaring a Function
+
+To declare a function you use the function keyword followed by the function name, a list of parameters, and the funciton body.
+
+function functionName( parameters) {
+    function body
+
+    CODE GOES HERE
+}
+*/
+
+function greeting( name ) {
+    console.log{ name };
+}
