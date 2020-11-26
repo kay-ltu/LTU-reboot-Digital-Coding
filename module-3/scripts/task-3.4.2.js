67a13c79337366e1ce3862e671057f060633bc3e
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-26 10:42:09 +0000

    Update task-3.4.2.js
    console log

--- module-3/scripts/task-3.4.2.js
+++ module-3/scripts/task-3.4.2.js
@@ -1,3 +1,8 @@
 var myFavouriteFoods = ['avocado', 'tomato', 'mozarella', 'apples', 'nuts'];
 
+for(var index = 0; index < myFavouriteFoods.length; index++ ){
 
+    console.log( myFavouriteFoods[index] );
+}
+
+//we could say my fave foods <= 5 by counting them manually, but if use.length as the list grows or shrinks, it calculates it automatically correctly. myFavouriteFoods.length vs myFavouriteFoods <=5
