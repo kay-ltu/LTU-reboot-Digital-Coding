e4b5ff0b7ac5a56cd42e813631ccfd0b462a42ca
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-26 11:10:39 +0000

    Create task-3.4.4.js

---
+++ module-3/scripts/task-3.4.4.js
@@ -0,0 +1,18 @@
+var user = {
+    name: 'Luke',
+    age: 36,
+    height: '5ft 11'
+}
+
+user.nameuser['name']
+
+
+var user = {
+    "fullname": 'Luke',
+    age: 36,
+    height: '5ft 11'
+}
+
+user.nameuser['name']
+user[" full name"]
+
