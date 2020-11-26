d8025a47ba91957d6df2d2635f1692480704b08c
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-26 10:45:20 +0000

    Update task-3.4.3.js
    Added document.write

---
+++ module-3/scripts/task-3.4.3.js
@@ -0,0 +1,8 @@
+var myFavouriteFoods = ['avocado ', 'tomato ', 'mozarella ', 'apples ', 'nuts '];
+
+for(var index = 0; index < myFavouriteFoods.length; index++ ){
+
+    document.write( myFavouriteFoods[index] );
+}
+
+
