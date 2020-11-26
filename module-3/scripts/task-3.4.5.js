ded4cd8ed5711251bc6d7a9e32553ae51ffdd5da
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-26 13:22:20 +0000

    Update task-3.4.5.js

---
+++ module-3/scripts/task-3.4.5.js
@@ -0,0 +1,22 @@
+var favouriteRecipe = {
+    recipeTitle: 'Yorkshire Puds',
+    servings: 4,
+    ingredients: ['2 eggs, ', 'half cup of milk, ', 'half cup of plain flour, ', 'salt, ', 'pepper'],
+    directions: ['Add all to a bowl, whisk and pour in a hot oiled tin for 20 mins at 220 degrees'],
+
+
+
+    //document.write( favouriteRecipe.recipeTitle);
+   // document.write( favouriteRecipe.servings);
+   // document.write( favouriteRecipe.ingredients);
+    //document.write( favouriteRecipe.directions);
+
+
+
+letscook: function()  {
+    console.log("I'm hungry lets cook " + favouriteRecipe.recipeTitle );
+}
+    }
+
+    favouriteRecipe.letscook();
+
