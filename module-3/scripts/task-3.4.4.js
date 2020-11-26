95d1bbbe444cbd4da06a33513d6209895640cad5
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-26 11:40:42 +0000

    Update task-3.4.4.js
    added console log

--- module-3/scripts/task-3.4.4.js
+++ module-3/scripts/task-3.4.4.js
@@ -1,9 +1,21 @@
 var favouriteRecipe = {
-    recipeTitle: 'Yorkshire Puds'
+    recipeTitle: 'Yorkshire Puds',
     servings: 4,
-    ingredients: ['2 eggs', 'half cup of milk', 'half cup of plain flour', 'salt', 'pepper'],
+    ingredients: ['2 eggs, ', 'half cup of milk, ', 'half cup of plain flour, ', 'salt, ', 'pepper'],
     directions: 'Add all to a bowl, whisk and pour in a hot oiled tin for 20 mins at 220 degrees'
 }
 
 
+    //document.write( favouriteRecipe.recipeTitle);
+   // document.write( favouriteRecipe.servings);
+   // document.write( favouriteRecipe.ingredients);
+    //document.write( favouriteRecipe.directions);
+
+
+var ingredients = favouriteRecipe.ingredients;
+var recipeTitle = favouriteRecipe.recipeTitle;
+
+for(var index = 0; index < ingredients.length; index ++ ){
+    console.log(ingredients[index] );
+    }
 
