52078c5aaa253ae867ca6e187d7897c5b52f962b
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-26 14:29:10 +0000

    Create task-3.5.3.js

---
+++ module-3/scripts/task-3.5.3.js
@@ -0,0 +1,87 @@
+let shoppingCart = [
+    {
+    name:"loaf of bread",
+    type:"food",
+    quantity:1,
+    price:.85
+    },
+    {
+    name:"multipack beans",
+    type:"food",
+    quantity:1,
+    price:1
+    },
+    {
+    name:"mushrooms",
+    type:"food",
+    quantity:10,
+    price:.1
+    },
+    {
+    name:"can of beer",
+    type:"alcohol",
+    quantity:4,
+    price:1.1
+    },
+    {
+    name:"prosecco",
+    type:"alcohol",
+    quantity:1,
+    price:8.99
+    },
+    {
+    name:"steak",
+    type:"food",
+    quantity:2,
+    price:3.99
+    },
+    {
+    name:"blue cheese",
+    type:"food",
+    quantity:1,
+    price:2.99
+    },
+    {
+    name:"candles",
+    type:"home",
+    quantity:3,
+    price:1.99
+    },
+    {
+    name:"cheesecake",
+    type:"food",
+    quantity:1,
+    price:4.99
+    },
+    {
+    name:"onions",
+    type:"food",
+    quantity:3,
+    price:.4
+    }];
+
+    function discount( arr, discountAmount, type ) { 
+
+        let totalPrice = 0; 
+
+        for ( let index = 0; index < arr.length; index++ )  {
+
+           
+
+
+
+
+        }
+
+        return totalPrice.toFixed(2); 
+
+    }
+    console.log(discountfood (shoppingCart));
+
+
+
+
+
+
+
+    
