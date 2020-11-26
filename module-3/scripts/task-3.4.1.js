c60950e6a8906c1155120c391c325f474b0d43fd
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-26 10:25:08 +0000

    Update task-3.4.1.js
    7 times table

---
+++ module-3/scripts/task-3.4.1.js
@@ -0,0 +1,10 @@
+// loop the 7 times table, on the proviso of 7 x 1, 7 x 2 to a max of 12 x 7, the var would be 1 as next it would be 2 and so on.
+for (var sevenTimes = 1; sevenTimes <=12; sevenTimes++ )
+{
+    var number = 7;
+
+    var result = sevenTimes * number;
+
+        console.log( number + ' * ' + sevenTimes + ' = ' + result );
+}
+
