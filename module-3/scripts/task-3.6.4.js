7c7a5aa7ac4fdea10c16a0a91187a01d41d08184
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-27 13:46:38 +0000

    Create task-3.6.4.js
    disable default link and pop up

---
+++ module-3/scripts/task-3.6.4.js
@@ -0,0 +1,10 @@
+//target and defaults
+
+
+var button = document.getElementById("myBtn");
+button.addEventListener("mouseover", function (event) {
+event.preventDefault();
+alert("Sorry no link here!");
+});
+
+
