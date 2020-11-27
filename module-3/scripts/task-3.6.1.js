30465a076c24467958ff4fcad8419d33cc15dbfc
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-27 11:42:38 +0000

    Update task-3.6.1.js

--- module-3/scripts/task-3.6.1.js
+++ module-3/scripts/task-3.6.1.js
@@ -16,4 +16,4 @@ console.log( image.alt) ;//in console show the alt text
 console.log( image.src) ;//in console show the source
 
 image.src= 'https://images.unsplash.com/photo-1606139053437-2551834526cd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8cVBZc0R6dkpPWWN8fGVufDB8fHw%3D&auto=format&fit=crop&w=500&q=60';//replace the image source
-image.alt = apple pie;//replace the image source
+
