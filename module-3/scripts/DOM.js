837c009cc02a5eb6a04e4f9e15303e561f27d120
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-27 10:32:50 +0000

    Update DOM.js
    add syntax and pulled info into console - change pic also

--- module-3/scripts/DOM.js
+++ module-3/scripts/DOM.js
@@ -1,13 +1,19 @@
 let heading1 = document.querySelector('h1');
-console.log( heading1) ;
+//console.log( heading1) ;
 
 let paragraph = document.querySelector('p');
-console.log( paragraph) ;
+//console.log( paragraph) ;
 
-let listItems = document.querySelectorAll('li')
+let listItems = document.querySelectorAll('li');
 
-console.log( listItems[1]) ;
+//console.log( listItems[1]) ;
 
-let itemClass = document.querySelectorAll('item')
+let itemClass = document.querySelectorAll('item');
 
-console.log( itemClass) ;
+//console.log( itemClass) ;
+
+let image = document.getElementById('image')
+console.log( image.alt) ;//in console show the alt text
+console.log( image.src) ;//in console show the source
+
+image.src= 'https://images.unsplash.com/photo-1606139053437-2551834526cd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8cVBZc0R6dkpPWWN8fGVufDB8fHw%3D&auto=format&fit=crop&w=500&q=60';//replace the image source
