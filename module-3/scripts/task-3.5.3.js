4181a86510ede8b4fc171974b9fda6b32a0d1188
kay-ltu <74243427+kay-ltu@users.noreply.github.com>
2020-11-30 14:26:35 +0000

    Update task-3.5.3.js
    Computer crashed midst this task so it got interrupted.
    
    Without access to recording, managed to get there eventually, from other resources, notes and old tasks.
    
    Added comments to help note taking.

--- module-3/scripts/task-3.5.3.js
+++ module-3/scripts/task-3.5.3.js
@@ -60,28 +60,56 @@ let shoppingCart = [
     price:.4
     }];
 
-    function discount( arr, discountAmount, type ) { 
 
-        let totalPrice = 0; 
+//Apply a discount of 20% to 'type:food' items only
+function discountFood( arr ) { 
 
-        for ( let index = 0; index < arr.length; index++ )  {
+    let totalPrice = 0; 
 
-           
+    for ( let index = 0; index < arr.length; index++ )  {
 
+       if ( arr[index].type === 'food' ) {
+         let discount = ( arr[index].price * 20 ) / 100;
 
+         totalPrice = totalPrice + (arr[index].price - discount ) * arr[index].quantity; 
+       
+       } else {
 
+        totalPrice = totalPrice + (arr[index].price * arr[index].quantity)
+            }
 
-        }
+     }
+    return totalPrice.toFixed(2); 
 
-        return totalPrice.toFixed(2); 
-
-    }
-    console.log(discountfood (shoppingCart));
-
-
-
+}
 
 
 
 
     
+//get the cart total with discount
+    function discountAmount( arr, discountAmount, type ) { //the function discount sets parameters for the array, the disocunt amount and the type of goods
+
+        let totalPrice = 0;  //starting at 0
+
+        for ( let index = 0; index < arr.length; index++ )  { //loop the changeable index point each time checking if reached the bottom of the list, reading each entry in turn
+
+           if (arr[ index ].type === type ) { //if each item has a type then...
+               let discount = ( arr[ index ].price * discountAmount ) / 100; //a viariable of discount can apply, looking at the array and to times the price by the discount amount
+            
+           
+totalPrice = totalPrice + ( arr  [index ].price - discount ) * arr[index].quantity; //The total price is now total price of the array prices minus the discount times by the quantity of each item in the shopping list if people ordered more than one of each.
+
+           } else if ( type === 'any') {
+               let discount = ( arr[ index ].price * discountAmount) / 100;
+               totalPrice = totalPrice + ( arr[ index ].price - discount ) * arr[ index ].quantity;
+           }else {
+               totalPrice=totalPrice+(arr[index].price*arr[index].quantity);
+           }
+        }
+        return totalPrice.toFixed(2);  // return the total price in two decimal places.
+
+    }
+  
+    let message = 'The cost of all your items comes to: £'
+console.log(message + discountAmount( shoppingCart, 20, 'any')); //Return discount amount function, stating the discount amount, applying to any type.
